5b99e759d35437d30ac9eab92523a855fe376944
kevin mattison and pablo vera <mattisonii@yahoo.com>
2016-01-12 14:16:17 -0800

    Add capitalization functionality to form submit.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $("form#capitalForm").submit(function(event) {
     var uppercaseInput = $("input#uppercase").val();
+    var uppercase = uppercaseInput.toUpperCase();
 
-    $(".uppercase").text(uppercaseInput);
+    $(".uppercase").text(uppercase);
 
     $("#hidden").show();
 
