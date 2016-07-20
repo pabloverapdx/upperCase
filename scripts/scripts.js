f31ec862f7c7d09c4f9615c3b6681c0df8896464
Pablo Vera <pablovera.pdx@gmail.com>
2016-07-20 14:05:45 -0700

    fix indentantion of all project from week 2 of intro to programming class

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
   $("form#capitalForm").submit(function(event) {
     var uppercaseInput = $("input#uppercase").val();
     var uppercase = uppercaseInput.toUpperCase();
-
     $(".uppercase").text(uppercase);
-
     $("#hidden").show();
-
     event.preventDefault();
   });
 });
